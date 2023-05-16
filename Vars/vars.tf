656adab6670389414f372e9d1c9fbcf110bc7240
purushothampudurthikkaiah <purushothampt@gmail.com>
2023-05-15 20:03:59 -0500

    output

--- Vars/vars.tf
+++ Vars/vars.tf
@@ -5,3 +5,28 @@ variable "example" {
 output "example" {
   value = var.example
 }
+
+#Data Types
+variable "sample1" {
+  default = "Hello Hello"
+}
+
+variable "sample2" {
+  default = 999
+}
+
+variable "sample3" {
+  default = true
+}
+
+output "DataType1" {
+  value = var.sample1
+}
+
+output "DataType2" {
+  value = var.sample2
+}
+
+output "DataType3" {
+  value = var.sample3
+}
